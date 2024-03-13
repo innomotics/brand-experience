0b407bbdf6cec08aedcc38b3d423ae50ba120566
ad001\z003a0vy <Istvan.Herbak@evosoft.com>
2024-03-13 11:26:16 +0100

    extending user interactions by redirecting outer click

--- packages/stencil-lib/src/components/inno-input/inno-input.tsx
+++ packages/stencil-lib/src/components/inno-input/inno-input.tsx
@@ -6,12 +6,14 @@ import { State, Prop, Component, Host, h } from '@stencil/core';
   scoped: true,
 })
 export class InnoInput {
+
+  private inputElementRef? : HTMLInputElement;
+
   @Prop({ mutable: true }) isActive: boolean;
   @Prop({ mutable: true }) isFocused: boolean;
   @Prop() label: string;
   @Prop() variant: string;
   @State() value: string;
-
 
   onBlur() {
     if (this.value === '' || this.value === undefined) {
@@ -32,12 +34,16 @@ export class InnoInput {
     this.isFocused = false;
   }
 
+  activateInput(){
+  this.inputElementRef.focus();
+  }
+
   render() {
     return (
       <Host>
-        <div class={{"input-container":true, "isactive":this.isActive, "focused": this.isFocused, "light": this.variant === "light", "dark": this.variant === "dark"}}>
+        <div class={{"input-container":true, "isactive":this.isActive, "focused": this.isFocused, "light": this.variant === "light", "dark": this.variant === "dark"}} onClick={()=> this.activateInput()}>
           <span class={{ label: true, float: this.isActive }}>{this.label}</span>
-          <input class={{"light": this.variant === "light", "dark": this.variant === "dark"}} onBlur={() => this.onBlur()} onFocus={() => this.onFocus()} onFocusout={() => this.onFocusout()} value={this.value} onInput={event => this.inputChanged(event)} />
+          <input ref={el => this.inputElementRef = el as HTMLInputElement} class={{"light": this.variant === "light", "dark": this.variant === "dark"}} onBlur={() => this.onBlur()} onFocus={() => this.onFocus()} onFocusout={() => this.onFocusout()} value={this.value} onInput={event => this.inputChanged(event)} />
         </div>
       </Host>
     );
