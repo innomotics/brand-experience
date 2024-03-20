4ff36bbec8da0e0ab59ceb79c51e37861db903fe
Denes Tamko <Denes.Tamko@evosoft.com>
2024-03-20 08:13:54 +0100

    Build fix.

--- packages/angular-lib/public-api.ts
+++ packages/angular-lib/public-api.ts
@@ -6,3 +6,4 @@ export { DIRECTIVES } from './lib/stencil-generated';
 export * from './lib/stencil-generated/components';
 export * from './lib/stencil-generated/text-value-accessor';
 export * from './lib/stencil-generated/select-value-accessor';
+export * from './lib/stencil-generated/boolean-value-accessor';
