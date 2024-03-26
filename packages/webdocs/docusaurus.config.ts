5ff09fee8448f393fafb0598cb59c1b05d946a2d
Vasko-Szedlar, Gabor <gabor.vasko-szedlar@evosoft.com>
2024-03-26 14:49:32 +0100

    Deploy docs to Github pages

--- packages/webdocs/docusaurus.config.ts
+++ packages/webdocs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://innomotics-ix-stage.westeurope.cloudapp.azure.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/IX/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
