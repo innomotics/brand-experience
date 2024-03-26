d4da42deb8b8efb298a634e1dd7624b9683a6b65
Vasko-Szedlar, Gabor <gabor.vasko-szedlar@evosoft.com>
2024-03-26 10:12:12 +0100

    Fix inno-error type to accept undefined

--- packages/stencil-lib/src/components.d.ts
+++ packages/stencil-lib/src/components.d.ts
@@ -83,7 +83,7 @@ export namespace Components {
     }
     interface InnoError {
         "active": boolean;
-        "type": 'badInput' | 'customError' |'patternMismatch' | 'rangeOverflow' |'rangeUnderflow' | 'stepMismatch' | 'tooLong' | 'tooShort' | 'typeMismatch' | 'valid' | 'valueMissing';
+        "type": 'badInput' | 'customError' |'patternMismatch' | 'rangeOverflow' |'rangeUnderflow' | 'stepMismatch' | 'tooLong' | 'tooShort' | 'typeMismatch' | 'valid' | 'valueMissing' | undefined;
     }
     /**
      * Represents the general footer for the Innomotics applications.
@@ -496,7 +496,7 @@ declare namespace LocalJSX {
     }
     interface InnoError {
         "active"?: boolean;
-        "type"?: 'badInput' | 'customError' |'patternMismatch' | 'rangeOverflow' |'rangeUnderflow' | 'stepMismatch' | 'tooLong' | 'tooShort' | 'typeMismatch' | 'valid' | 'valueMissing';
+        "type"?: 'badInput' | 'customError' |'patternMismatch' | 'rangeOverflow' |'rangeUnderflow' | 'stepMismatch' | 'tooLong' | 'tooShort' | 'typeMismatch' | 'valid' | 'valueMissing' | undefined;
     }
     /**
      * Represents the general footer for the Innomotics applications.
--- packages/stencil-lib/src/components/inno-error/inno-error.tsx
+++ packages/stencil-lib/src/components/inno-error/inno-error.tsx
@@ -8,7 +8,7 @@ import { Component, Host, Prop, h } from '@stencil/core';
 export class InnoError {
 
   @Prop() active : boolean = false;
-  @Prop() type: 'badInput' | 'customError' |'patternMismatch' | 'rangeOverflow' |'rangeUnderflow' | 'stepMismatch' | 'tooLong' | 'tooShort' | 'typeMismatch' | 'valid' | 'valueMissing';
+  @Prop() type: 'badInput' | 'customError' |'patternMismatch' | 'rangeOverflow' |'rangeUnderflow' | 'stepMismatch' | 'tooLong' | 'tooShort' | 'typeMismatch' | 'valid' | 'valueMissing' | undefined;
   
   // @Watch('active')
   // activehandling(newValue: boolean)
