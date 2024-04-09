bb6ecc7c5358a757906e75a832a98cfe934eb58a
iherbak <istvan.herbak@evosoft.com>
2024-04-09 09:22:25 +0200

    export order change for proper dist generation of angular lib

--- packages/angular-lib/public-api.ts
+++ packages/angular-lib/public-api.ts
@@ -2,9 +2,9 @@
  * Public API Surface of components
  */
 export * from "@innomotics/ix";
-export * from "./lib/components.module";
-export { DIRECTIVES } from "./lib/stencil-generated";
-export * from "./lib/stencil-generated/components";
 export * from "./lib/stencil-generated/select-value-accessor";
 export * from "./lib/stencil-generated/boolean-value-accessor";
 export * from "./lib/components/inno-radio/inno-radio-value-accessor";
+export * from "./lib/components.module";
+export { DIRECTIVES } from "./lib/stencil-generated";
+export * from "./lib/stencil-generated/components";
