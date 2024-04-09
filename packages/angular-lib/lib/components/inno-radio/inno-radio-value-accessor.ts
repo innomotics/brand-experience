632257499afdd731d4585b08d2775872fa06f364
patrik.kmetcz <patrik.kmetcz@evosoft.com>
2024-04-09 09:00:08 +0200

    cleanup

--- packages/angular-lib/lib/components/inno-radio/inno-radio-value-accessor.ts
+++ packages/angular-lib/lib/components/inno-radio/inno-radio-value-accessor.ts
@@ -1,6 +1,16 @@
 import { NG_VALUE_ACCESSOR, RadioControlValueAccessor } from "@angular/forms";
 import { Directive } from "@angular/core";
 
+/**
+ * Custom value accessor to InnoRadio element
+ * to synchronize the actual state between the form elements.
+ *
+ * The class extends the built-in radio accessor which provides
+ * the same functionality like it does for input[type=radio] elements
+ * but it does not provide total access to the underlying properties
+ * and methods. If this is required then a custom accessor should be
+ * implemented.
+ */
 @Directive({
   selector:
     "inno-radio[formControlName],inno-radio[formControl],inno-radio[ngModel]",
