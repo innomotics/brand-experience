80be4433ae785a08bb9baedb7c8cf6d6199fb565
Herbák István <istvan.herbak@evosoft.com>
2024-09-17 11:12:29 +0200

    reverting

--- packages/test-applications/angular-test-app/src/app/app.module.ts
+++ packages/test-applications/angular-test-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from '@innomotics/brand-experience-angular-lib';
+import { ComponentsModule } from '@innomotics/brand-experience-angular-lib/dist';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FormExampleComponent } from './components/form-example/form-example.component';
 import { GeneralExampleComponent } from './components/general-example/general-example.component';
