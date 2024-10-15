d26710db5006be3f04da3b30d22de0582c80d518
Denes Tamko <Denes.Tamko@evosoft.com>
2024-10-15 10:44:28 +0200

    Added new icons. Updated some old ones.

--- packages/icon-lib/generator.ts
+++ packages/icon-lib/generator.ts
@@ -21,12 +21,10 @@ const styleRegexes: RegExp[] = [
 
 // these files are using explicitly style to achive the effects
 const passModifications: string[] = [
-  "globe",
   "Worldwide",
   "sort-down2",
   "sort-up-down2",
   "sort-up2",
-  "trash"
 ];
 
 //passsing directoryPath and callback function
